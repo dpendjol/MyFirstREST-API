7cf6e94404a259d1894d91fc0da62e73267fbe3a
Dominique Pendjol <d.pendjol@gmail.com>
2021-03-31 20:19:33 +0200

    made routes for songs

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 const cors = require('cors');
+const fs = require('fs');
+const key = fs.readFileSync('../key.pem');
+const cert = fs.readFileSync('../cert.pem');
+const https = require('https');
+const app = express();
+const server = https.createServer({key: key, cert: cert}, app);
+
 require('dotenv').config('~/.env');
 
-const app = express();
-
 user = process.env.db_user
-passwd = process.env.db_pass
+pass = process.env.db_pass
 host = process.env.db_host
 port = process.env.db_port
 db = process.env.db_name
@@ -16,13 +21,11 @@ app.set('view engine', 'ejs');
 
 app.use(cors());
 
-app.use(express.json({
-   
-}));
+app.use(express.json({}));
 
 app.use(express.static("public"));
-connection_string = 
-mongoose.connect(`mongodb://${user}:${pass}@${host}:${port}/${db}?authSource=${db}`, { useNewUrlParser: true, useUnifiedTopology: true})
+
+connection_string = mongoose.connect(`mongodb://${user}:${pass}@${host}:${port}/${db}?authSource=${db}`, { useNewUrlParser: true, useUnifiedTopology: true})
 	.then(() => console.log('connecting'))
 	.catch(err => console.error('something went wrong', err));
 
@@ -36,21 +39,54 @@ todoItem.set("timestamps", {
     updatedAt: "_updatedOn"
 });
 
+const songItemSchema = new mongoose.Schema({
+    title: String,
+    artist: String,
+    genre: String,
+    rating: Number,
+});
+
+songItem.set("timestamps", {
+    createdAt: "_createdOn",
+    updatedAt: "_updatedOn"
+});
+
 const todoItem = mongoose.model('TodoItem', todoItemSchema)
+const songItem = mongoose.model('SongItem', songItemSchema)
+
+/* TODO LIST ITEM */
 
 app.get('/api', function(req, res){
     /*
         getting data from the API
     */
-    todoItem.find(function(err, result) {
+
+    let queryResult = ''
+    if (req.query.sort) {
+        const sorting = req.query.sort;
+        if (sorting[0] === '-') {
+            field = sorting.slice(1)
+
+            sortBy = {[field]: -1}
+        } else {
+
+            sortBy = {[sorting]: 1}
+        }
+        queryResult = todoItem.find().sort(sortBy)
+    } else {
+        queryResult = todoItem.find()
+    }
+
+    queryResult.exec(function(err, result) {
         if (!err) {
-	    console.log(result)
+            console.log(result)
             res.send(result);
         } else {
             res.send(err);
         }
     })
 });
+
 
 app.post('/api', (req, res) => {
     /*
@@ -61,11 +97,11 @@ app.post('/api', (req, res) => {
     const newItem = new todoItem(req.body);
 
     newItem.save(function(err, result){
-      if (!err) {
-        res.send(result);
-      } else {
-        res.send(err);
-      }
+        if (!err) {
+            res.send(result);
+        } else {
+            res.send(err);
+        }
     })
 });
 
@@ -78,8 +114,6 @@ app.delete('/api', function(req, res) {
         }
     })
 });
-
-/////////////////// SPECIFIC ITEMS
 
 app.route('/api/:id')
     .get(function(req, res) {
@@ -114,8 +148,106 @@ app.route('/api/:id')
         });
     });
 
-app.get('/api')
+/********** Songs ****************/
+
+app.get('/songs', function(req, res){
+    /*
+        getting data from the API
+    */
+
+    let queryResult = ''
+    if (req.query.sort) {
+        const sorting = req.query.sort;
+        if (sorting[0] === '-') {
+            field = sorting.slice(1)
+
+            sortBy = {[field]: -1}
+        } else {
+
+            sortBy = {[sorting]: 1}
+        }
+        queryResult = songItem.find().sort(sortBy)
+    } else {
+        queryResult = songItem.find()
+    }
+
+    queryResult.exec(function(err, result) {
+        if (!err) {
+            res.send(result);
+        } else {
+            res.send(err);
+        }
+    })
+});
+
+
+app.post('/songs', (req, res) => {
+    /*
+    Handeling post request to the server
+    */
+    console.log(req.body);
+
+    const newItem = new songItem(req.body);
+
+    newItem.save(function(err, result){
+        if (!err) {
+            res.send(result);
+        } else {
+            res.send(err);
+        }
+    })
+});
+
+app.delete('/songs', function(req, res) {
+    songItem.deleteMany({}, function(err, result) {
+        if (!err) {
+            res.send(result);
+        } else {
+            res.send(err);
+        }
+    })
+});
+
+/* SPECIFIC ITEMS */
+
+app.route('/songs/:id')
+    .get(function(req, res) {
+        songItem.findOne({_id: req.params.id}, function(err, result) {
+            if (result) {
+                res.send(result);
+            } else {
+                res.send("Nothing found")
+            }
+        });
+    })
+    .put(function(req, res){
+        songItem.update({_id: req.params.id}, req.body, {overwrite: true}, function(err, result) {
+            if (!err) {
+                res.send('Correcty updated')
+            }
+        })
+    })
+    .patch(function(req, res){
+        songItem.update({_id: req.params.id}, {$set: req.body}, function(err, result) {
+            if (!err) {
+                res.send('Correcty updated')
+            }
+        })
+    })
+    .delete(function(req, res) {
+        myid = req.params.id;
+        songItem.deleteOne({_id: req.params.id}, function(err) {
+            if (!err) {
+                res.send('Deleted item')
+            }
+        });
+    });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+server.listen(3001, function() {
+    console.log("Server started on port 3000");
+  });
+
